a5517961897f1db8371aabede07119919595dddf
Jonathan CR <jonathan.ribeiro@grupow.com.br>
2021-08-06 13:25:55 -0300

    finished

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,43 @@
-export function Content() {
-  // Complete aqui
+import React from 'react';
+import { MovieCard } from './MovieCard';
+export interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
 }
+export interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+interface ContentProps {
+  movies: MovieProps[];
+  selectedGenre: GenreResponseProps;
+}
+
+const Content: React.FC<ContentProps> = ({ movies, selectedGenre}) => {
+  // Complete aqui
+
+  return (
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard key={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export default Content;
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,31 @@
-export function SideBar() {
-  // Complete aqui
+import React from 'react';
+import { Button } from './Button';
+import { GenreResponseProps } from './Content';
+
+interface SideBarProps {
+  genres: GenreResponseProps[];
+  selectedGenreId: number;
+  callbackChangeGenres(id: number): void;
 }
+
+const SideBar: React.FC<SideBarProps> = ({ genres, selectedGenreId, callbackChangeGenres }) => {
+  // Complete aqui
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => callbackChangeGenres(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  )
+}
+export default SideBar;
